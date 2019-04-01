e1e4bb2db120c8494dc9457548cc7d9fe7f6aa4f
Adetunji <ashennaike@gmail.com>
2019-04-01 16:15:39 -0400

    basic setup starting now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 const App = () => (
   <div>
--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import as router
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  rootElement
+);
